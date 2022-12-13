4168b5761e35ccebb80d7d3473d2d8146bd92461
caio lucena <caio.lcolaco@gmail.com>
2022-12-13 14:40:01 -0300

    adjust splash screen while fonts are loading

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
+import React, { useCallback } from 'react';
 import {StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const  [fonteCarregada] = useFonts({
+  // Loading fonts
+  const  [fontsLoaded] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold
   })
 
-  if(!fonteCarregada){
-    return <View />
+  // Configurations to show a loader while fonts are loading
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
+
+  // General Return
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta />
     </SafeAreaView>
